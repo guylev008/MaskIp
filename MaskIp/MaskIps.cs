29f8fa2fc21906780748917f5b04daa245d25475
guy <guy.selenium@gmail.com>
2020-12-05 15:58:04 +0200

    first commit

---
+++ MaskIp/MaskIps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MaskIp
+{
+    public interface IMaskIps
+    {
+        string Mask(string i_ip);
+    }
+    public class MaskIps : IMaskIps
+    {
+        private string _networkAddress;
+        private readonly Lazy<Random> _rand = new Lazy<Random>();
+
+        public string Mask(string i_ip)
+        {
+            if (string.IsNullOrEmpty(_networkAddress))
+            {
+                _networkAddress = $"{_rand.Value.Next(1, 256)}.{_rand.Value.Next(1, 256)}.{_rand.Value.Next(1, 256)}";
+            }
+
+            return $"{_networkAddress}.{_rand.Value.Next(1, 256)}";
+        }
+    }
+
+    public enum IpAddressClass
+    {
+        ClassC = 110
+    }
+}
